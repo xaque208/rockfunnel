6a81eae550ba3c6f919a9ebf08d8c2f415bae6bf
Zach Leslie <zach@puppetlabs.com>
2012-08-19 20:26:28 -0700

    add graphite class for keeping network connections

--- lib/rockfunnel.rb
+++ lib/rockfunnel.rb
@@ -5,5 +5,22 @@ require 'redis'
 module RockFunnel
   module_function
 
-  
+  class Graphite
+    def initialize(server,port)
+      @server, @port = server, port
+      @s = TCPSocket.open(server,port)
+    end
+
+    def send (target,value,time)
+      line = "#{target} #{value} #{time}"
+      puts line
+      @s.puts(line)
+    end
+
+    def close
+      @s.close
+    end
+  end
+
+
 end
